48bbdf95df28ace4218ca07ce4a04ef5cb1a78fe
sarthak <sarthak.ahuja0007@gmail.com>
2021-06-18 20:29:43 +0530

    adding firebase

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDeBq0kPyjeP2TWgDns4zOY7bOZJUqWNBo",
+    authDomain: "whatsapp-b3ecc.firebaseapp.com",
+    projectId: "whatsapp-b3ecc",
+    storageBucket: "whatsapp-b3ecc.appspot.com",
+    messagingSenderId: "631261586939",
+    appId: "1:631261586939:web:d7b90a9fd49cde16add75e",
+    measurementId: "G-R8P1SJLWYH"
+  };
