9d02cd0c8a291f48ac31baa76860c4620e856556
sarthak <sarthak.ahuja0007@gmail.com>
2021-06-19 15:31:08 +0530

    created main component

---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Main(props) {
+    return (
+        <div>
+            {/* sidebar */}
+            {/* Chat */}
+        </div>
+    )
+}
+
+export default Main
