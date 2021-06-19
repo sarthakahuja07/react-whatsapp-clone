ccd93ce236754a55a3d61534838e8b736a25e2e2
sarthak <sarthak.ahuja0007@gmail.com>
2021-06-20 01:13:51 +0530

    sidebar done

--- src/components/SidebarChatComponent.js
+++ src/components/SidebarChatComponent.js
@@ -2,12 +2,17 @@ import React from 'react'
 import Avatar from '@material-ui/core/Avatar';
 
 function SidebarChat() {
+
+	var messgae="loe haa hamh aekdmngalgka gik naugbghab fgh dhsadha hahah ahaha"
 	return (
 		<div className="sidebar-chat">
 			<Avatar alt="Remy Sharp" src="https://avatars.dicebear.com/api/human/sarthak.svg" />
 			<div className="info">
-				<h2 className="info-name">Name</h2>
-				<p>Message</p>
+				<h2 className="info-name">Name</h2> 
+					{/* TODO: 
+					Add a character Limit to mesaage and name
+					*/}
+				<p>{messgae}</p>
 			</div>
 
 		</div>
