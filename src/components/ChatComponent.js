d1313474adff66d04704f7a03373ddef5e54ab6d
sarthak <sarthak.ahuja0007@gmail.com>
2021-06-19 16:21:48 +0530

    completed app-body

---
+++ src/components/ChatComponent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Chat() {
+    return (
+        <div>
+            <p>this is chat</p>
+        </div>
+    )
+}
+
+export default Chat;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import Sidebar from './SidebarComponent';
+import Chat from './ChatComponent';
+import '../css/mainComponent.css';
 
 function Main(props) {
     return (
-        <div>
-            {/* sidebar */}
-            {/* Chat */}
+        <div className="app-body-container">
+        <div className="app-body">
+            <Sidebar />
+            <Chat />
+        </div>
+
         </div>
     )
 }
